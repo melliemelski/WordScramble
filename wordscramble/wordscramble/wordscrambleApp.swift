770ce42e33c5cfb98912115f995fa86bb814eba8
Mel Sargent <42639343+melliemelski@users.noreply.github.com>
2022-02-01 10:57:54 +0000

    Add files via upload

---
+++ wordscramble/wordscramble/wordscrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordscrambleApp.swift
+//  wordscramble
+//
+//  Created by Mel on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct wordscrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
